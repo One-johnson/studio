b59fad6070371cd1bb6c0e937f63e5bbe630b051
PRINCE JOHNSON <princeonejohnson@gmail.com>
2025-09-05 08:56:39 +0000

    what we have is not looking good, the colors don't match the bg colors.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,20 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
+import { Alegreya, Belleza } from 'next/font/google';
+
+const belleza = Belleza({
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--font-belleza',
+});
+
+const alegreya = Alegreya({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  variable: '--font-alegreya',
+});
 
 export const metadata: Metadata = {
   title: 'SnapVerse - Modern Photography Portfolio',
@@ -13,13 +27,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Belleza:ital,wght@0,400;0,700;1,400&family=Alegreya:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
-      </head>
-      <body className="font-body antialiased">
+    <html lang="en" className={`${belleza.variable} ${alegreya.variable} dark`} suppressHydrationWarning>
+      <body>
         {children}
         <Toaster />
       </body>
