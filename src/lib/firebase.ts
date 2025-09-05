781ad0360640a205733dcaf19d849b46f168f4b7
PRINCE JOHNSON <princeonejohnson@gmail.com>
2025-09-05 05:08:20 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  projectId: "snapverse-803lq",
-  appId: "1:298314408533:web:bd1bbf0406ff19e189eadc",
-  storageBucket: "snapverse-803lq.appspot.com",
-  apiKey: "AIzaSyD-0akA5rLDrnFQu1_HJv4LxE84fwu5np8",
-  authDomain: "snapverse-803lq.firebaseapp.com",
-  messagingSenderId: "298314408533",
+  "projectId": "snapverse-803lq",
+  "appId": "1:298314408533:web:bd1bbf0406ff19e189eadc",
+  "storageBucket": "snapverse-803lq.firebasestorage.app",
+  "apiKey": "AIzaSyD-0akA5rLDrnFQu1_HJv4LxE84fwu5np8",
+  "authDomain": "snapverse-803lq.firebaseapp.com",
+  "messagingSenderId": "298314408533",
 };
 
 // Initialize Firebase
