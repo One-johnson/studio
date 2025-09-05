7f820776bfe5938b35722f712eba08e324fffeeb
PRINCE JOHNSON <princeonejohnson@gmail.com>
2025-09-05 04:01:20 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  projectId: "snapverse-803lq",
+  appId: "1:298314408533:web:bd1bbf0406ff19e189eadc",
+  storageBucket: "snapverse-803lq.firebasestorage.app",
+  apiKey: "AIzaSyD-0akA5rLDrnFQu1_HJv4LxE84fwu5np8",
+  authDomain: "snapverse-803lq.firebaseapp.com",
+  messagingSenderId: "298314408533",
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+
+export { app, db };
